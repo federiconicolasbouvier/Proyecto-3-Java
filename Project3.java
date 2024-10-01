2a1467eb8583298e073dc6bf87ce7b5865d94de3
Federico Nicolas Bouvier <136862169+federiconicolasbouvier@users.noreply.github.com>
2024-10-01 18:07:58 -0300

    Add files via upload

---
+++ Project3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bouvier.project3;
+
+/**
+ *
+ * @author Bouvier, Federico Nicolás
+ */
+public class Project3 {
+
+    public static void main(String[] args) {
+        Project3GUI v = new Project3GUI();
+        v.setVisible(true);
+    }
+}
